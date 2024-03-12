b928447db724f1b96aec0d909b14037e22860784
nullpointerexpected <juniorprogrammer09@gmail.com>
2024-03-13 02:00:04 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      {" "}
+      <h1>Hello</h1>
+    </main>
+  );
+}
+
+export default App;
